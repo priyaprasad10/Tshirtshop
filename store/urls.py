07b5f8f5bb75a5bac663ab775f530580a016e919
priyaprasad10 <priyapriyanka2002@gmail.com>
2021-02-10 16:59:50 +0530

    init

---
+++ store/urls.py
@@ -0,0 +1,21 @@
+
+
+from django.contrib import admin
+from django.urls import path
+from store.views  import home,cart,LoginView,signout,signup,add_to_cart,checkout
+from store.views  import validatePayment,ProductDetailView,OrderListView
+from django.contrib.auth.decorators import login_required
+urlpatterns = [
+
+    path('', home,name='homepage'),
+    path('cart/',cart),
+    path('orders/',login_required(OrderListView.as_view(),login_url='/login/'),name='orders'),
+    path('login/',LoginView.as_view(), name='login'),
+    path('signup/',signup,),
+    path('logout/',signout,),
+    path('product/<str:slug>',ProductDetailView.as_view()),
+    path('addtocart/<str:slug>/<str:size>',add_to_cart),
+    path('checkout/',checkout),
+    path('validate_payment',validatePayment)
+   
+]
---
+++ store/views/orders.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,HttpResponse , redirect
+from store.forms.authforms import CustomerCreationForm,CustomerAuthForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate ,login as loginUser ,logout
+from store.models import Tshirt,SizeVariant,Cart,Order,OrderItem,Payment,Occasion,Brand,Color,IdealFor,NeckType,Sleeve
+from math import floor
+from store.forms.checkout_form import CheckForm
+
+from django.views.generic.list import ListView
+
+
+
+
+class OrderListView(ListView):
+   template_name='store/orders.html'
+   model=Order
+   paginate_by=1
+   context_object_name='orders'
+   def get_queryset(self):
+      return Order.objects.filter(user=self.request.user).order_by('-date').exclude(order_status='PENDING')
